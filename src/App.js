5ab4bf0f5ee1d23f5303eb893bc9f49ecf0e959a
Christine <christine.altmann@gmx.com>
2019-10-29 15:15:28 +0000

    setup form to capture input data on submit resolve#2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  // on submit capture input data
+  const handleSubmit = e => {
+    e.preventDefault();
+    const searchBusinessName = e.target.elements.businessName.value;
+    console.log(searchBusinessName);
+  };
+
   return (
-    <div className="App">
-      <p>Hello World</p>
-    </div>
+    <section className="business-lookup__form">
+      <h1>Welcome to my business lookup app</h1>
+      <form id="business-lookup__form" onSubmit={handleSubmit}>
+        <label htmlFor="businessName">Please enter a business name</label>
+        <input
+          className="businessName"
+          id="businessName"
+          type="text"
+          required
+        />
+        <input type="submit" value="search" />
+      </form>
+    </section>
   );
 }
 
