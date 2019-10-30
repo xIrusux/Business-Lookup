c0fa8cd3cb5405ef7319be25cac16c52b7e10a9a
Christine <christine.altmann@gmx.com>
2019-10-30 23:49:50 +0000

    add script via useEffect now works in theory and looks for callback function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import "./App.css";
-import { getBusinessMatch } from "./utils/getBusinessMatch";
+// import { getBusinessMatch } from "./utils/getBusinessMatch";
 import { useEffect } from "react";
-import { exportNamedDeclaration } from "@babel/types";
 
 function App() {
   const [input, setInput] = React.useState("");
@@ -15,20 +14,35 @@ function App() {
     const url = `https://abr.business.gov.au/json/MatchingNames.aspx?name=${input}&maxResults=10&callback=callback&guid=${GUID}`;
 
     const script = document.createElement("script");
+    script.type = "text/javascript";
     script.src = url;
   };
 
-  const callback = nameData => {
-    for (var i = 0; i < nameData.Names.length; i++) {
-      console.log(
-        nameData.Names[i].Abn +
-          " " +
-          nameData.Names[i].Name +
-          " " +
-          nameData.Names[i].Score
-      );
-    }
-  };
+  useEffect(() => {
+    const jasonpScript = document.createElement("script");
+    const GUID = process.env.REACT_APP_GUID;
+
+    jasonpScript.src = `https://abr.business.gov.au/json/MatchingNames.aspx?name=department+of+industry&maxResults=10&callback=callback&guid=${GUID}`;
+    jasonpScript.type = "text/javascript";
+
+    document.body.appendChild(jasonpScript);
+
+    const callback = nameData => {
+      console.log(nameData);
+      for (var i = 0; i < nameData.Names.length; i++) {
+        console.log(
+          nameData.Names[i].Abn +
+            " " +
+            nameData.Names[i].Name +
+            " " +
+            nameData.Names[i].Score
+        );
+      }
+    };
+    return () => {
+      document.body.removeChild(jasonpScript);
+    };
+  }, []);
 
   return (
     <section className="business-name-form">
